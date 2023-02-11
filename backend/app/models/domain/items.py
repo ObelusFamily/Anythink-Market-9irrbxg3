edf1724536fa481a0cf4065a1184608c7b328f9c
WilcoApp <hello@wilco.gg>
2023-02-11 14:16:29 +0000

    Route to Victory

--- backend/app/models/domain/items.py
+++ backend/app/models/domain/items.py
@@ -1,7 +1,5 @@
 from typing import List, Optional
 
-from pydantic import validator
-
 from app.models.common import DateTimeModelMixin, IDModelMixin
 from app.models.domain.profiles import Profile
 from app.models.domain.rwmodel import RWModel
@@ -17,7 +15,3 @@ class Item(IDModelMixin, DateTimeModelMixin, RWModel):
     favorites_count: int
     image: Optional[str]
     body: Optional[str]
-
-    @validator('image', pre=True, always=True)
-    def set_default_image(cls, v):
-        return v or '/placeholder.png'
